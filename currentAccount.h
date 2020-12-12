32b598d5bfba19895cb85fae2a632e93d59ba0e9
renanmoura6941 <naneraruom@gmail.com>
2020-12-12 10:05:14 -0300

    currentAccount struct

---
+++ currentAccount.h
@@ -0,0 +1,8 @@
+
+typedef struct currentAccount{
+    char name[30];
+    int agency;
+    int account;
+    double balance;
+
+}CurrentAccount;
--- main.c
+++ main.c
@@ -1,1 +1,7 @@
+#include <stdio.h>
+#include "currentAccount.h"
 
+int main(){
+    printf("Hello world");
+   
+}
